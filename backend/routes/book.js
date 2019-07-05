227847e552fabfeae529e21c6200db6931e53b83
Javier <javiercaraballo97@gmail.com>
2019-07-05 18:41:54 -0300

    adding routes get post delete

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -1,18 +1,42 @@
-const {Router}=require('express');
+const { Router } = require('express');
 
-const router=Router();
-const Book= require('../models/Book');
+const router = Router();
+const Book = require('../models/Book');
 
 
 
-router.get('/', async (req,res)=>{
+router.get('/', async (req, res) => {
     const books = await Book.find();// creo que es el find all de sequelize
     res.json(books);
 
 });
 
-//router.post()
+router.post('/create', async (req, res) => {
 
+    const newBook = new Book(req.body);
+    console.log(newBook);
+    await newBook.save();
+
+    res.json(
+        {
+            message: "book saved",
+            data: newBook
+        }
+    )
+
+
+
+});
+
+router.delete('/:id', async (req, res) => {
+    const {id} = req.params
+    const bookDeleted = await Book.findByIdAndDelete(id);
+    res.json({
+        message:"this book was deleted",
+        data:bookDeleted
+
+    })
+});
 
 
 module.exports = router;
